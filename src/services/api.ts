a3a35a42c7a506e823457af92068e3ca5edcc106
Sebastian Amaya <sebasamayaxd@hotmail.com>
2025-05-21 19:11:16 -0500

    Develop (#10)
    * character service, structure and spinner
    
    * adding views, functionality and configurations before styles
    
    * [200~ styles in components applied and improved, main functionality added, fixing problems like to back to actual page and no to first
    
    * using vue toast library for show visual erros to user when the app fail (#3)
    
    Co-authored-by: sebasam <=>
    
    * Feature/unit testing app (#4)
    
    * testing CharacterDetail
    
    * CharacterCards component tests works
    
    * store/characters tests completed
    
    * unit testing for main home component ok
    
    ---------
    
    Co-authored-by: sebasam <=>
    
    * complement HomeView and CharacterDetail tests, and i added new test for components like main, App and api (#5)
    
    Co-authored-by: sebasam <=>
    
    * implementation for a basical integrate test in HomeView, file HomeWiew.integration.spec.ts tested (#6)
    
    Co-authored-by: sebasam <=>
    
    * deleting unused icons, adding test script and short icon (#7)
    
    Co-authored-by: sebasam <=>
    
    * removing api url and moving it in a environment variable (#9)
    
    Co-authored-by: sebasam <=>
    
    ---------
    
    Co-authored-by: sebasam <=>

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import type { Character } from '../types/character'
 
-const BASE_URL = import.meta.env.VITE_API_URL || 'https://rickandmortyapi.com/api'
+
+const BASE_URL = import.meta.env.VITE_API_URL
 
 async function request<T>(endpoint: string): Promise<T> {
   try {
